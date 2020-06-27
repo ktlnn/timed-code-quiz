5287cab56869704e04ac5a4e090bde076529bb07
Katelynn Nguyen <64226204+ktlnn@users.noreply.github.com>
2020-06-27 19:47:34 -0400

    added timer

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,49 @@
+
+var minutesDisplay = document.querySelector("#minutes");
+var secondsDisplay = document.querySelector("#seconds");
+var startButton = document.querySelector("#start-button");
+var mainContent = document.querySelector("#main-content");
+
+var secondsElapsed = 0;
+var totalSeconds = 0;
+var interval;
+
+console.log("something");
+
+
+
+
+
+
+
+
+
+function runClockCb(){
+    // increasing seconds elapsed by 1 
+    secondsElapsed++;
+    console.log(secondsElapsed);
+    // displaly minutes remaining. used "Math.floor" so there are no decimals
+    minutesDisplay.textContent = Math.floor((totalSeconds - secondsElapsed) / 60);
+    secondsDisplay.textContent = (totalSeconds - secondsElapsed) % 60;
+    
+}
+
+function startTimer(){
+    document.getElementById("#main-content").style.display = "block";
+    document.getElementById("#start-button").style.display = "block";
+    var minutes = 10;
+    totalSeconds = minutes * 60;
+    secondsElapsed = 0;
+    if(typeof interval !== "undefined"){
+        clearInterval(interval);
+    }
+    interval = setInterval(runClockCb, 1000);
+}
+
+
+
+startButton.addEventListener("click", startTimer);
+
 var questionList = [
     {
         "q": ""
